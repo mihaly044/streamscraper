4972a3c80c34e21d3a02960f38ede38f35b28896
Mihaly Meszaros <mihaly044@gmail.com>
2018-09-02 16:00:18 +0200

    Implemented project

---
+++ Parsers/ParserFactory.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using stream_downloader;
+
+namespace streamscraper
+{
+    public static class ParserFactory
+    {
+        private static IDictionary<string, Type> _parsers;
+
+        public static void RegisterParser<T>(string abbrevation) where T : IParser
+        {
+            if(_parsers == null)
+                _parsers = new Dictionary<string, Type>();
+
+            if(_parsers.ContainsKey(abbrevation))
+                return;
+
+            _parsers.Add(abbrevation, typeof(T));
+
+        }
+
+        public static IParser GetParser(string key)
+        {
+            if(_parsers == null || !_parsers.ContainsKey(key))
+                return null;
+
+            return (IParser)Activator.CreateInstance(_parsers[key]);
+        }
+    }
+}
---
+++ Parsers/Tv2Parser.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using stream_downloader;
+
+namespace streamscraper
+{
+    public class Tv2Parser: IParser
+    {
+        public async Task<string> ParseAsync(string uri)
+        {
+            var html = await Downloader.DownloadString(uri);
+            var splitted = html.Split(new[] {"var jsonUrl = \"//"}, StringSplitOptions.None);
+            var jsonUrl = "http://" + splitted[1].Substring(0, splitted[1].IndexOf(';')-1);
+
+            var jsonResponse = await Downloader.DownloadString(jsonUrl);
+            var tv2Response = Tv2Response.FromJson(jsonResponse);
+
+            return "http:" + tv2Response.Bitrates.Hls;
+        }
+    }
+
+    public partial class Tv2Response
+    {
+        [JsonProperty("bitrates")]
+        public Bitrates Bitrates { get; set; }
+
+        [JsonProperty("backupBitRates")]
+        public BackupBitrates BackupBitrates { get; set; }
+
+        [JsonProperty("mp4Labels")]
+        public List<string> Mp4Labels { get; set; }
+
+        [JsonProperty("bitrateType")]
+        public string BitrateType { get; set; }
+
+        [JsonProperty("sourceType")]
+        public string SourceType { get; set; }
+
+        [JsonProperty("videoId")]
+        public string VideoId { get; set; }
+
+        [JsonProperty("servers")]
+        public List<string> Servers { get; set; }
+    }
+
+    public class Bitrates
+    {
+        [JsonProperty("hls")]
+        public string Hls { get; set; }
+    }
+
+    public class BackupBitrates
+    {
+        [JsonProperty("hls")]
+        public string Hls { get; set; }
+    }
+
+    public partial class Tv2Response
+    {
+        public static Tv2Response FromJson(string json) => JsonConvert.DeserializeObject<Tv2Response>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Tv2Response self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    public static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None
+        };
+    }
+}
