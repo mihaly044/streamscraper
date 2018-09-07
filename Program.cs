1922d66cbc6494fea910b960ab5b227fb587b76e
Mihaly Meszaros <mihaly044@gmail.com>
2018-09-07 16:39:28 +0200

    Removed buggy progress bar (linux)

--- Program.cs
+++ Program.cs
@@ -105,47 +105,11 @@ namespace streamscraper
 
             _downloader.OnProgress += (progress, time) =>
             {
-                DrawTextProgressBar(progress, 100);
+                Console.Write("Time={0} Complete={1}%\t\r", time, progress);
             };
             _downloader.OnDownloadComplete += () => ConsoleKit.Message(ConsoleKit.MessageType.INFO, 
                 "Download complete!\n");
             _downloader.DownloadStream(parsedUri, savepath);
-        }
-
-        private static void DrawTextProgressBar(int progress, int total)
-        {
-            //draw empty progress bar
-            Console.CursorLeft = 0;
-            Console.Write("["); //start
-            Console.CursorLeft = 32;
-            Console.Write("]"); //end
-            Console.CursorLeft = 1;
-            float onechunk = 30.0f / total;
-
-            //draw filled part
-            int position = 1;
-            for (int i = 0; i < onechunk * progress; i++)
-            {
-                Console.BackgroundColor = ConsoleColor.Gray;
-                Console.CursorLeft = position++;
-                Console.Write(" ");
-            }
-
-            //draw unfilled part
-            for (int i = position; i <= 31; i++)
-            {
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.CursorLeft = position++;
-                Console.Write(" ");
-            }
-
-            //draw totals
-            Console.CursorLeft = 35;
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write(progress.ToString() + "%    "); //blanks at the end remove any excess
-
-            if(progress == total)
-                Console.WriteLine();
         }
     }
 }
