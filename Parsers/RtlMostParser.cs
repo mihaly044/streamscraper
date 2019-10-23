841a624b95b3f8b1684c0b0459b417e76d4631cd
mihaly044 <mihaly044@gmail.com>
2019-10-23 17:00:57 +0200

    Updated rtlmost parser

--- Parsers/RtlMostParser.cs
+++ Parsers/RtlMostParser.cs
@@ -8,15 +8,15 @@ namespace streamscraper
         public async Task<string> ParseAsync(string uri)
         {
             var html = await Downloader.DownloadString(uri);
-            var splitted = html.Split(new[] { "\"full_physical_path\":\"" }, StringSplitOptions.None);
+            var splitted = html.Split(new[] { "\\\"full_physical_path\\\":\\\"" }, StringSplitOptions.None);
 
             var foundMedia = (from chunk in splitted
-            where chunk.StartsWith("http")
+            where chunk.StartsWith("https")
             select chunk.Split('"')[0].Replace("\\u002F", "/")).ToList();
 
             foreach (var url in foundMedia.Where(url => url.Contains("_unpnp.ism/")))
             {
-                return url;
+                return url.Substring(0, url.Length-1);
             }
 
             return "";
